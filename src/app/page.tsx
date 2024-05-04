90db5344f93cc5671a13afedc22817522a4137ce
temporraryaccounthasbeensuspended <wweziza@icloud.com>
2024-05-05 02:11:55 +0700

    final

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,28 +4,29 @@ import styles from './page.module.css';
 
 const Home = () => {
   return (
-    <div className={styles.container}>
-      <nav className={styles.navbar}>
-        <ul className={styles.navLinks}>
-          <li>LSW.</li>
-        </ul>
-      </nav>
-      <main className={styles.main}>
-        <div className={styles.textContainer}>
-          <h1>Don't miss the vibe</h1>
-          <p>you think it's lorem ipsum? pee-poop bro, the string telling you to discover timeless fashion that empowers your confidence.</p>
-          {/* Use Link for button */}
-          <a href="https://instagram.com/lsw.ink" className={styles.shopBtn}>Touch Us
-          </a>
-        </div>
-        <div className={styles.imageContainer}>
-          <img src="image/display.webp" alt="Clothing" />
-        </div>
-      </main>
+    <div>
+      <div className={styles.container}>
+        <nav className={styles.navbar}>
+          <ul className={styles.navLinks}>
+            <li>LSW.</li>
+          </ul>
+        </nav>
+        <main className={styles.main}>
+          <div className={styles.textContainer}>
+            <h1>Don't miss the vibe</h1>
+            <p>you think it's lorem ipsum? pee-poop bro, the string telling you to discover timeless fashion that empowers your confidence.</p>
+            {/* Use Link for button */}
+            <a href="https://instagram.com/lsw.ink" className={styles.shopBtn}>Touch Us</a>
+          </div>
+          <div className={styles.imageContainer}>
+            <img src="image/display.webp" alt="Clothing" />
+          </div>
+        </main>
+      </div>
       <footer className={styles.footer}>
         <div className={styles.footerDisclaimer}>
           <p>&copy; 2024 LSW. All rights reserved.</p>
-          <p>Designed by YourName</p>
+          <p>Designed by weziza</p>
         </div>
       </footer>
     </div>
